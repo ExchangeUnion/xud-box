b5c84b64e04985207bacd45ca8fbe76c78b1fed7
Karl Ranna <karl@karlranna.com>
2018-11-14 22:31:44 +0200

    Add check-deps

---
+++ check-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Check Virtualbox
+temp=$(which VBoxManage)
+if [ $? -ne 0 ]; then
+	echo "Virtualbox is missing"
+	exit 1
+fi
+# Check Vagrant
+temp=$(which vagrant)
+if [ $? -ne 0 ]; then
+	echo "Vagrant is missing"
+	exit 1
+fi
