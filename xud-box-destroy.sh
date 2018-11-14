4eb113d67fee0ddfe1d274b54bb9f6e37864704f
Karl Ranna <karl@karlranna.com>
2018-11-14 22:03:29 +0200

    Refactor check-deps

--- xud-box-destroy.sh
+++ xud-box-destroy.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Check Virtualbox
-temp=$(which VBoxManage)
-if [ $? -ne 0 ]; then
-	echo "Virtualbox is missing"
-	exit 1
-fi
-# Check Vagrant
-temp=$(which vagrant)
-if [ $? -ne 0 ]; then
-	echo "Vagrant is missing"
-	exit 1
-fi
+./check-deps.sh
 vagrant destroy -f
